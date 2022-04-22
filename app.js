306671c36c5dedcdc94d31304f61999365ff81f8
zonaetmunna <zonaetmonna@gmail.com>
2022-04-22 19:54:56 +0600

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+const path=require('path');
+
+// path baseName
+const myPath='D:/course-web development-with -jankar sir-part2/node.js-learn-with-sumit/node.js/app.js';
+console.log(path.basename(myPath));
+
+// path dirName
+console.log(path.dirname(myPath));
+
+// path delimeter
+console.log(path.delimiter)
